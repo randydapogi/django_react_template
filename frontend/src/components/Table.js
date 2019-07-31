d376d041a69ff884182a046d6d771ed734714d33
randydapogi <rcberos@up.edu.ph>
2019-07-31 20:55:50 +0800

    first commit

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -12,20 +12,6 @@ const Table = ({ data }) =>
       <h2 className="subtitle">
         Showing <strong>{data.length} items</strong>
       </h2>
-      {/* <table className="table is-striped">
-        <thead>
-          <tr>
-            {Object.entries(data[0]).map(el => <th key={key(el)}>{el[0]}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(el => (
-            <tr key={el.id}>
-              {Object.entries(el).map(el => <td key={key(el)}>{el[1]}</td>)}
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
       <MDBTable btn>
       <MDBTableHead>
         <tr>
